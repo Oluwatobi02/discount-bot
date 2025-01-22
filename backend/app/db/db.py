ae402ba39f295f98085e92e1a237b97391c9bf4f
Oluwatobi <tobiolajide01@gmail.com>
2025-01-21 20:10:56 -0500

    []

--- backend/app/db/db.py
+++ backend/app/db/db.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 from app.builders.user_builder import UserBuilder
 from app.builders.product_builder import ProductBuilder
-from user_activity_builder import UserActivityBuilder
+from backend.app.builders.user_activity_builder import UserActivityBuilder
 class Database:
     __instance = None
     connected = False
